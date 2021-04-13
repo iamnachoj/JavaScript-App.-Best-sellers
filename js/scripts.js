e279ef1e0e4ada4d13546d91e0505d184441dbf1
Ignacio Jimenez <nachojimenez.94@gmail.com>
2021-04-13 19:29:14 +0200

    add 12 objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: "Bulbasaur", height: 62.55, type:["grass", "poison"]};
+  {name: "Charmander", height: 60.96, type: "fire"};
+  {name: "Squirtel", height: 30.48, type:"water"};
+  {name: "Caterpie", height: 29.24, type:"bug"};
+  {name: "Weedle", height: 29.24, type:["bug", "poison"]};
+  {name: "Pidgey", height: 31.24, type:["normal", "flying"]};
+  {name: "Rattata", height: 27.24, type:"normal"};
+  {name: "Spearow", height: 30.14, type:["normal", "flying"]};
+  {name: "Ekans", height: 184.88, type:"poison"};
+  {name: "Pikachu", height: 28.47, type:"electric"};
+  {name: "Sandshrew", height: 34.57, type:"ground"};
+  {name: "Nidoran", height: 27.57, type:"poison"};
+];
