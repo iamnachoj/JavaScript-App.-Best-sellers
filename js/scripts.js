8f4b5fdbda5903949b43662ced6a6efd471f319b
Ignacio Jimenez <nachojimenez.94@gmail.com>
2021-04-15 12:49:52 +0200

    add loop with conditionals for pokemonList array to show in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ let pokemonList = [
   {name: "Sandshrew", height: 34.57, type:["ground"]},
   {name: "Nidoran", height: 27.57, type:["poison"]},
 ];
+
+for(i = 0; i < pokemonList.length; i++){
+  document.write("<p> " + pokemonList[i].name + ", (" + pokemonList[i].height + "cm).");
+  if(pokemonList[i].height > 100){
+    document.write(" - " + pokemonList[i].name + " is a very big one!. ");
+  };
+  document.write("<p>")
+}
