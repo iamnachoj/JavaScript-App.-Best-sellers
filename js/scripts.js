8dc4f15ab2758ca5d93d22a6bbba8af46a1b1fc8
Ignacio Jimenez <nachojimenez.94@gmail.com>
2021-04-23 16:15:15 +0200

    add IIFE named pokemonRepository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-let pokemonList = [
-  {name: "Bulbasaur", height: 62.55, type:["grass", "poison"]},
-  {name: "Charmander", height: 60.96, type: ["fire"]},
-  {name: "Squirtel", height: 30.48, type:["water"]},
-  {name: "Caterpie", height: 29.24, type:["bug"]},
-  {name: "Weedle", height: 29.24, type:["bug", "poison"]},
-  {name: "Pidgey", height: 31.24, type:["normal", "flying"]},
-  {name: "Rattata", height: 27.24, type:["normal"]},
-  {name: "Spearow", height: 30.14, type:["normal", "flying"]},
-  {name: "Ekans", height: 184.88, type:["poison"]},
-  {name: "Pikachu", height: 28.47, type:["electric"]},
-  {name: "Sandshrew", height: 34.57, type:["ground"]},
-  {name: "Nidoran", height: 27.57, type:["poison"]},
-];
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {name: "Bulbasaur", height: 62.55, type:["grass", "poison"]},
+    {name: "Charmander", height: 60.96, type: ["fire"]},
+    {name: "Squirtel", height: 30.48, type:["water"]},
+    {name: "Caterpie", height: 29.24, type:["bug"]},
+    {name: "Weedle", height: 29.24, type:["bug", "poison"]},
+    {name: "Pidgey", height: 31.24, type:["normal", "flying"]},
+    {name: "Rattata", height: 27.24, type:["normal"]},
+    {name: "Spearow", height: 30.14, type:["normal", "flying"]},
+    {name: "Ekans", height: 184.88, type:["poison"]},
+    {name: "Pikachu", height: 28.47, type:["electric"]},
+    {name: "Sandshrew", height: 34.57, type:["ground"]},
+    {name: "Nidoran", height: 27.57, type:["poison"]},
+  ];
 
 function myLoopPokemon(pokemon){
   document.write("<p> " + pokemon.name + ", (" + pokemon.height + "cm).");
@@ -22,3 +23,5 @@ function myLoopPokemon(pokemon){
 }
 
 pokemonList.forEach(myLoopPokemon);
+
+})()
