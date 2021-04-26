a1f54f88b7cde41ada0104b9f1679c0cf251a371
Ignacio Jimenez <nachojimenez.94@gmail.com>
2021-04-26 12:51:03 +0200

    add new function within the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -32,20 +32,23 @@ let pokemonRepository = (function(){
     return pokemonList;
   }
 
+  function addListItem(pokemon){
+    let list = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "Placeholder";
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+  }
+
   return{
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
 pokemonRepository.getAll().forEach(function myLoopPokemon(pokemon) {
-  let list = document.querySelector(".pokemon-list");
-  let listItem = document.createElement("li");
-  let button = document.createElement("button");
-  button.innerText = "Placeholder";
-  button.classList.add("button-class");
-  listItem.appendChild(button);
-  list.appendChild(listItem);
-
-
+pokemonRepository.addListItem(pokemon);
 });
