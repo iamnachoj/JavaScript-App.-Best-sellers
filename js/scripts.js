1b5b891e8d565de21f89f69ff6f015664e8e3c7f
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-05-06 19:33:37 +0200

    add function loadList

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,22 @@ let pokemonRepository = (function(){
     return pokemonList;
   }
 
-
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+  
   return{
     add: add,
     getAll: getAll,
